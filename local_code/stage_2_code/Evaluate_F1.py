798f4db5dc6d9168869fd18ffc6856578640fac5
Samuel Li <samuelli4521@gmail.com>
2025-04-25 03:07:09 -0700

    sam initial commit

---
+++ local_code/stage_2_code/Evaluate_F1.py
@@ -0,0 +1,18 @@
+'''
+Concrete Evaluate class for a specific evaluation metrics
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from local_code.base_class.evaluate import evaluate
+from sklearn.metrics import f1_score
+import numpy as np
+
+
+class Evaluate_F1(evaluate):
+    data = None
+    
+    def evaluate(self):
+        return f1_score(self.data['true_y'], self.data['pred_y'], average="weighted")
+        
---
+++ local_code/stage_2_code/Evaluate_Precision.py
@@ -0,0 +1,17 @@
+'''
+Concrete Evaluate class for a specific evaluation metrics
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from local_code.base_class.evaluate import evaluate
+from sklearn.metrics import precision_score
+
+
+class Evaluate_Precision(evaluate):
+    data = None
+    
+    def evaluate(self):
+        return precision_score(self.data['true_y'], self.data['pred_y'], average="weighted")
+        
